ab81d1ff1f8c53269fc4004946796619b9e16b60
DucTrungDo <ducdt2192@gmail.com>
2023-11-19 22:20:34 +0700

    Create routes crud for User review

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,98 +1,103 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name:{
+  name: {
+    type: String,
+    required: [true, 'Please enter product name'],
+    trim: true,
+    maxLength: [100, 'Product name cannot exceed 100 characters'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Please enter product price'],
+    maxLength: [10, 'Product price cannot exceed 5 characters'],
+    default: 0.0,
+  },
+  description: {
+    type: String,
+    required: [true, 'Please enter product descriptions'],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
         type: String,
-        required: [true, 'Please enter product name'],
-        trim: true,
-        maxLength: [100, 'Product name cannot exceed 100 characters']
-    },
-    price:{
-        type: Number,
-        required: [true, 'Please enter product price'],
-        maxLength: [10, 'Product price cannot exceed 5 characters'],
-        default: 0.0
-    },
-    description:{
+        required: true,
+      },
+      url: {
         type: String,
-        required: [true, 'Please enter product descriptions'],
+        required: true,
+      },
     },
-    ratings:{
-        type: Number,
-        default:0
+  ],
+  category: {
+    type: String,
+    required: [true, 'Please select category for this product'],
+    enum: {
+      values: [
+        'Electronics',
+        'Cameras',
+        'Laptops',
+        'Accessories',
+        'Headphones',
+        'Food',
+        'Books',
+        'Clothes/Shoes',
+        'Beauty/Health',
+        'Sport',
+        'Outdoor',
+        'Home',
+      ],
+      message: 'Please select correct category for product',
     },
-    images:[
-        {
-            public_id: {
-                type: String,
-                required: true
-            },
-            url: {
-                type: String,
-                required: true
-            },
-        }
-    ],
-    category:{
-        type: String,
-        required: [true, 'Please select category for this product'],
-        enum: {
-            values: [
-                'Electronics',
-                'Cameras',
-                'Laptops',
-                'Accessories',
-                'Headphones',
-                'Food',
-                'Books',
-                'Clothes/Shoes',
-                'Beauty/Health',
-                'Sport',
-                'Outdoor',
-                'Home'
-            ],
-            message: 'Please select correct category for product'
-        }
-    },
-    seller: {
-        type: String,
-        required :[true, 'Please enter product seller']
-    },
-    stock: {
-        type: Number,
-        required: [true, 'Please enter product stock'],
-        maxLength: [5, 'Product name cannot exceed 5 characters'],
-        default: 0
-    },
-    numOfReviews:{
-        type: Number,
-        default: 0
-    },
-    reviews:[
-        {
-            name:{
-                type: String,
-                required: true
-            },
-            rating:{
-                type: String,
-                required: true
-            },
-            comment:{
-                type: String,
-                required: true
-            }
-        }
-    ],
-    user:{
+  },
+  seller: {
+    type: String,
+    required: [true, 'Please enter product seller'],
+  },
+  stock: {
+    type: Number,
+    required: [true, 'Please enter product stock'],
+    maxLength: [5, 'Product name cannot exceed 5 characters'],
+    default: 0,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      user: {
         type: mongoose.Schema.ObjectId,
         ref: 'User',
-        required: true
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: String,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
     },
-    createdAt:{
-        type: Date,
-        default: Date.now
-    }
-})
+  ],
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-module.exports = mongoose.model('Product', productSchema)
+module.exports = mongoose.model('Product', productSchema);
